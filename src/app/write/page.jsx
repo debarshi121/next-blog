f3aa5216724a6cefa26814a5031c914228de85ac
Debarshi Das <debarshi121@gmail.com>
2024-01-23 00:02:52 +0530

    write page

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
 
-const page = () => {
-	return <div>Write</div>;
+import React, {useState} from "react";
+import {FaImage, FaUpload} from "react-icons/fa";
+import {FaPlus} from "react-icons/fa6";
+import {MdOutlineOndemandVideo} from "react-icons/md";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const WritePage = () => {
+	const [open, setOpen] = useState(false);
+	const [value, setValue] = useState("");
+	return (
+		<div className="flex flex-col gap-5 mt-10">
+			<input type="text" className="border border-gray-300 rounded py-2 px-3" placeholder="Your awesome title here!" />
+			<div className="flex gap-5 mt-5">
+				<button onClick={() => setOpen(!open)} className="w-8 h-8 rounded-full border-2 cursor-pointer border-gray-400">
+					<FaPlus className="mx-auto my-auto w-5 h-5 text-gray-400" />
+				</button>
+				{open && (
+					<div className="flex gap-5">
+						<button className="w-8 h-8 rounded-full border-2 cursor-pointer border-green-500">
+							<FaImage className="mx-auto my-auto w-4 h-4 text-green-500" />
+						</button>
+						<button className="w-8 h-8 rounded-full border-2 cursor-pointer border-green-500">
+							<FaUpload className="mx-auto my-auto w-4 h-4 text-green-500" />
+						</button>
+						<button className="w-8 h-8 rounded-full border-2 cursor-pointer border-green-500">
+							<MdOutlineOndemandVideo className="mx-auto my-auto w-4 h-4 text-green-500" />
+						</button>
+					</div>
+				)}
+			</div>
+			<ReactQuill theme="snow" className="h-80" value={value} onChange={setValue} placeholder="Write your story here..." />
+		</div>
+	);
 };
 
-export default page;
+export default WritePage;
